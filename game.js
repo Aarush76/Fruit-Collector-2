75aa3935ea620683b1144f82e6ffcfd28736531a
Aarush76 <73341865+Aarush76@users.noreply.github.com>
2021-01-04 12:41:26 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,148 @@
+class Game{
+    constructor(){
+
+    }
+    getState() {
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value", function (data) {
+            gameState = data.val();
+        })
+
+    }
+
+    update(state) {
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+    async start() {
+            if (gameState === 0) {
+                player = new Player();
+                var playerCountRef = await database.ref('playerCount').once("value");
+                if (playerCountRef.exists()) {
+                    playerCount = playerCountRef.val();
+                    player.getCount();
+                }
+                form = new Form()
+                form.display();
+            }
+    player1 = createSprite(200,500);
+    player1.addImage("player1",player_img);
+    
+    player2 = createSprite(800,500);
+    player2.addImage("player2", player_img);
+    players=[player1,player2];
+
+        }
+    
+    play(){
+        
+                form.hide();
+
+                Player.getPlayerInfo();
+                 image(back_img, 0, 0, 1000, 800);
+                 var x =100;
+                 var y=200;
+                 var index =0;
+                 drawSprites(); 
+                 for(var plr in allPlayers){
+                    
+                    
+                     index = index+1;
+                     x = 500-allPlayers[plr].distance;
+                     y=500;
+                     
+                     
+                     players[index -1].x = x;
+                     players[index - 1].y = y;
+
+                     //pl1Score = player1.score;
+                     //pl2Score = player2.score;
+                       
+                     if(index === player.index){
+                         
+                         fill("black");
+                         textSize(25);
+                         text(allPlayers[plr].name ,x-25,y+25);
+
+                         
+                     }
+                    
+                     fill('#E20000')
+                     textSize(20)
+                     text("Player 1: " + allPlayers.player1.score , 700, 70)
+                    
+                     fill('#E20000')
+                     textSize(20)
+                     text("Player 2: " + allPlayers.player2.score , 700, 120)
+                    
+                 
+                 }
+                
+                 //pl1Score =  players[0].score;
+                 //pl2Score =  players[1].score;
+                
+
+                if (keyIsDown(RIGHT_ARROW) && player.index !== null) {
+                    player.distance -= 10
+                    player.update();
+                }
+                if (keyIsDown(LEFT_ARROW) && player.index !== null) {
+                    player.distance += 10
+                    player.update();
+                }
+            
+                 if (frameCount % 40 === 0) {
+                     fruits = createSprite(random(100, 1000), 0, 100, 100);
+                     fruits.velocityY = 6;
+                     var rand = Math.round(random(1,5));
+                     switch(rand){
+                         case 1: fruits.addImage("fruit1",fruit1_img);
+                         break;
+                         case 2: fruits.addImage("fruit1", fruit2_img);
+                         break;
+                         case 3: fruits.addImage("fruit1", fruit3_img);
+                         break;
+                         case 4: fruits.addImage("fruit1", fruit4_img);
+                         break;
+                         case 5: fruits.addImage("fruit1", fruit5_img);
+                         break;
+                     }
+                     fruitGroup.add(fruits);
+                     
+                 }
+
+              
+                 
+                  if (player.index !== null) {
+                     //fill code here, to destroy the objects.
+
+                    for(var i = 0; i<fruitGroup.length; i++)
+                     if(fruitGroup.get(i).isTouching(players)){
+                       fruitGroup.get(i).destroy();
+                       player.score++
+                       //players[0].score = players[0].score +10;
+                       console.log(player.score)
+                       player.update();
+                     }
+
+                    
+                   
+                }
+                
+                
+               
+                //fill('#E20000')
+                //textSize(20)
+                //text("Player 2: " + pl2Score, 700, 170)
+         
+         
+        
+         
+
+    }
+
+    end(){
+       console.log("Game Ended");
+    }
+}
